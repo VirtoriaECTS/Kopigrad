f3b11f8b7cc2d19cc687efce160758792ab050fa
VirtoriaECTS <vikmolotova@gmail.com>
2025-06-03 03:56:14 +0500

    до подключения куки

---
+++ Kopigrad/Components/Classes/Autorisation/AutorisationClasses.cs
@@ -0,0 +1,63 @@
+﻿using Kopigrad.Models;
+
+namespace Kopigrad.Components.Classes.Autorisation
+{
+    public class AutorisationClasses
+    {
+        private string code = "vbFHnc7t-A-Md.";
+
+        public void register(string nameUser, string email, string password, string code)
+        {
+            using(var context = new KopigradContext())
+            {
+                Classes.Autorisation.Hash hash = new Classes.Autorisation.Hash();
+                hash.Create(password);
+                string hashpawword = hash.HashValue;
+                string salt = hash.Salt;
+
+
+                var user = new Models.User()
+                {
+                    NameUser = nameUser,
+                    Email = email,
+                    HashPassworld = hashpawword,
+                    Salt = salt
+                };
+
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
+        }
+
+
+
+        public string CheakPassword(string email, string password)
+        {
+            using (var context = new KopigradContext())
+            {
+                var user = context.Users.Where(x => x.Email == email).FirstOrDefault();
+                if (user != null)
+                {
+                    Classes.Autorisation.Hash hash = new Classes.Autorisation.Hash();
+                    hash.HashValue = user.HashPassworld;
+                    hash.Salt = user.Salt;
+
+                    bool cheak = hash.Verify(password);
+                    if (cheak)
+                    {
+                        return "";
+                    }
+                    else
+                    {
+                        return "Неверный пароль";
+                    }
+                }
+                else
+                {
+                    return "Пользователь с данной почтой не найден";
+                }
+                
+            }
+        }
+    }
+}
---
+++ Kopigrad/Components/Classes/Autorisation/Hash.cs
@@ -0,0 +1,51 @@
+﻿namespace Kopigrad.Components.Classes.Autorisation
+{
+    using System;
+    using System.Security.Cryptography;
+
+    public class Hash
+    {
+
+        public string Salt { get;  set; }
+
+
+        public string HashValue { get;  set; }
+
+
+        public void Create(string password)
+        {
+            // 1) Генерируем случайную соль 16 байт
+            byte[] saltBytes = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(saltBytes);
+            }
+            Salt = Convert.ToBase64String(saltBytes);
+
+            // 2) Вычисляем PBKDF2-хэш пароля с этой солью
+            //    - 10000 итераций
+            //    - длина выходного ключа: 32 байта (256 бит)
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, 10000, HashAlgorithmName.SHA256))
+            {
+                byte[] hashBytes = pbkdf2.GetBytes(32);
+                HashValue = Convert.ToBase64String(hashBytes);
+            }
+        }
+
+        public bool Verify(string password)
+        {
+            if (string.IsNullOrEmpty(Salt) || string.IsNullOrEmpty(HashValue))
+                throw new InvalidOperationException("Соль или хэш не инициализированы.");
+
+            byte[] saltBytes = Convert.FromBase64String(Salt);
+
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, 10000, HashAlgorithmName.SHA256))
+            {
+                byte[] hashBytes = pbkdf2.GetBytes(32);
+                string computedHash = Convert.ToBase64String(hashBytes);
+                return computedHash == HashValue;
+            }
+        }
+    }
+
+}
